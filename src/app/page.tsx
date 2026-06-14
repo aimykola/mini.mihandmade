a534851b2f05312963ce6c5e56e123952c02f6f0
aimykola <aimykola15@gmail.com>
2026-06-14 22:57:39 +0300

    Use dynamic Contacts section from site_settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Header from '@/components/Header'
 import Catalog from '@/components/Catalog'
 import CartDrawer from '@/components/cart/CartDrawer'
-import { Hero, About, Reviews, Contacts, Footer } from '@/components/Sections'
+import { Hero, About, Reviews, Footer } from '@/components/Sections'
+import Contacts from '@/components/ContactsSection'
 
 export default function Home() {
   return (
